80ef443725f3e3b54eb27121559abec3daa78c85
Tim Kinnane <timkinnane@gmail.com>
2018-09-12 00:10:17 +1000

    💜 Heroku stuff

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const bot = require('bbot')
 
+/** This is a workaround for Heroku to confirm web binding. */
+// @todo Remove this when bBot includes Express (next release)
+const http = require('http')
+const handle = (req, res) => res.end('hit')
+const server = http.createServer(handle)
+server.listen(process.env.PORT || 5000)
+
 /** Add your bot logic here. Removing the imported examples. */
 require('./src/examples')
 
